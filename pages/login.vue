0c60bc703666a296c381ec3ba807095bdca327bd
Pornkamon <ita5908@ccollege.ac.th>
2020-07-10 13:51:26 +0700

    sent1

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+ <v-app>
+   <v-card>
+     <v-card-title>
+       <h1>Login</h1>
+     </v-card-title>
+     <v-card-text>
+       <v-form>
+         <v-text-field  prepend-icon="mdi-account-circle" label="Username" />
+         <v-text-field prepend-icon="https" append-icon="visibility_off" label="Password" />
+       </v-form>
+         <v-card-actions>
+        <nuxt-link to="/register"> 
+        <v-btn large color="success">REGISTER</v-btn>
+        </nuxt-link>
+         <v-spacer></v-spacer>
+        <v-btn large color="primary">LOGIN</v-btn>
+        </v-card-actions>
+     </v-card-text>
+   </v-card>
+ </v-app>
+</template>
