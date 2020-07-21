26a8a528dabb12adee6cc18187f985cd31d52c89
Pornkamon <ita5908@ccollege.ac.th>
2020-07-21 13:30:02 +0700

    gam

---
+++ layouts/then1.vue
@@ -0,0 +1,82 @@
+<template>
+    <!-- App.vue -->
+
+<v-app>
+   <v-system-bar dark color="red lighten-2">
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+    
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+<div>
+<v-app-bar>
+    <v-tabs
+      dark
+      background-color="primary"
+      grow
+    >
+      <v-tab>
+        <v-badge
+          color="pink"
+          dot
+        >
+          หน้าหลัก
+        </v-badge>
+      </v-tab>
+
+      <v-tab>
+        <v-badge
+          color="green"
+          content="6"
+        >
+          ประวัติ
+        </v-badge>
+      </v-tab>
+
+      <v-tab>
+        <v-badge
+          color="deep-purple accent-4"
+          icon="mdi-vuetify"
+        >
+          Login
+        </v-badge>
+      </v-tab>
+    </v-tabs>
+    </v-app-bar> 
+</div>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-content>
+      <nuxt/>
+    </v-content>
+  </v-main>
+
+  <v-footer>
+    <v-btn>
+      <span>Recents</span>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+
+  </v-footer>
+
+</v-app>
+</template>
---
+++ pages/home01.vue
@@ -0,0 +1,36 @@
+<template>
+    <div align="center">
+    <h1>ประวัติส่วนตัว</h1>
+        <br>
+    <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="250px"
+      src="https://scontent.fnak2-1.fna.fbcdn.net/v/t1.0-9/110322881_2966374346922769_1308616108307974075_o.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_eui2=AeHuIYq-G_oqAgLxDJdF0836ylruQTJQwMvKWu5BMlDAy9ocsbPtQ2RgmcIAIwV57aKpk_vJGbGaxflQdDnxHmYG&_nc_ohc=Li-Wyc51ieEAX8yREBF&_nc_ht=scontent.fnak2-1.fna&oh=51e52ca4a1a699c522bfe5c27c8f08a6&oe=5F3CD034"
+    >
+    </v-img>
+
+    <v-card-subtitle class="pb-0">ประวัติส่วนตัว</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>นางสาวพรกมล อยู่ภักดี</div>
+
+      <div>ปวส.2 E1</div>
+
+      <div>แผนกวิชาเทคโนโลยีสารสนเทศ</div>
+
+      <div>วิทยาลัยเทคนิคชัยภูมิ</div>
+    </v-card-text>
+  </v-card>
+  
+    </div>
+</template>
+
+<script>
+export default {
+    layout:"then1",
+}
+</script>
